8adf078491b059ed70cdc58f9cfa118d9d4707ef
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-16 23:15:41 -0600

    added timer, changed quizQuestions array, added forEach to display answer options

--- resources/script.js
+++ resources/script.js
@@ -1,62 +1,103 @@
 // variables
 var beginQuizBtn = document.querySelector(".beginQuiz");
 var viewScoresBtn = document.querySelector(".high-scores");
-var quizQuestion = document.querySelector(".question");
-var answerOptions = document.querySelector(".answer-options");
-var feedback = document.querySelector(".feedback");
+// var quizQuestion = document.querySelector(".question");
+// var answerOptions = document.querySelector(".answer-options");
+// var result = document.querySelector(".result");
+var questionSection = document.querySelector(".question-section");
+
+// TIMER
+var timeRemaining = document.querySelector(".time-remaining");
+var timer;
+var timerCount;
+
 var score = 0;
 
-// questions for quiz
+// Quiz Array
 var quizQuestions = [
     {
-        question: "1. Where is the correct place JavaScript link?",
-        choices: [
-            { option: "In the <body> section", answer: true },
-            { option: "In the <footer> section", answer: false },
-            { option: "After the <footer> section", answer: false }
-        ]
+        ask: "Where should you place the JavaScript link?",
+        choices: [{
+            a: "Where should you place the JavaScript link?",
+            b: "In the <footer> section",
+            c: "After the <footer> section",
+        }],
+        answer: "a"
     },
     {
-        question: "2. How does a FOR loop start?",
-        choices: [
-            { option: "for (i <= 5; i++)", answer: false },
-            { option: "for (i = 0; i <= 5)", answer: false },
-            { option: "for (i = 0; i <= 5; i++)", answer: true },
-        ]
+        ask: "How does a FOR loop start?",
+        choices: [{
+            a: "for (i <= 5; i++)",
+            b: "for (i = 0; i <= 5)",
+            c: "for (var i = 0; i <= 5; i++)"
+        }],
+        answer: "c"
     },
-    {
-        question: "3. Correct way to write a JavaScript array?",
-        choices: [
-            { option: "var colors = ['red', 'green', 'blue']", answer: true },
-            { option: "var colors = 'red', 'green', 'blue'", answer: false },
-            { option: "var colors = [1:'red', 2:'green', 3:'blue']", answer: false },
-        ]
-    }
 ];
 
 
+
+// console.log("quizQuestions:", quizQuestions);
+// console.log("ask:", quizQuestions[0].ask);
+// console.log("choices:", quizQuestions[0].choices);
+
 // Add event listeners
 beginQuizBtn.addEventListener("click", beginQuiz);
-viewScoresBtn.addEventListener("click", viewScores);
 
-
-// functions
+// Shuffle quiz questions
 function beginQuiz() {
-    console.log("begin button working")
-    beginQuizBtn.style.visibility = 'hidden';
-    var currentQuestion = 0;
+    timerCount = 10;
+    beginQuizBtn.disabled = true;
+    startTimer();
+    showQuestions();
+};
 
-    quizQuestion.textContent = quizQuestions[currentQuestion].question;
+function showQuestions() {
+    // Loop through and get each question
+    for (var i = 0; i < quizQuestions.length; i++) {
+        var questionTitle = quizQuestions[i].ask;
+        console.log("question:", questionTitle)
 
-      for (let i = 0; i < quizQuestions[currentQuestion].choices.length; i++){
-        var choiceBtn = document.createElement("button");
-        choiceBtn.textContent = quizQuestions[currentQuestion].choices[i].option;
+        var displayQuestion = document.createElement('h2');
+        displayQuestion.textContent = questionTitle;
+        questionSection.appendChild(displayQuestion);
+        console.log("choices:", quizQuestions[i].choices)
+        console.log("choices 2.0:", quizQuestions[i].choices[0])
 
-        answerOptions.appendChild(choiceBtn);
+        // make a button for each answer choice
+        quizQuestions[i].choices.forEach(function(option){
+            for (var value in option) {
+                // console.log(`${option[value]}`);
+
+                var displayChoices = document.createElement('div');
+                var choiceBtn = document.createElement('button');
+                choiceBtn.textContent = `${option[value]}`;
+
+                questionSection.appendChild(displayChoices);
+                questionSection.appendChild(choiceBtn);
+            }
+        })
     }
 }
 
-function viewScores() {
-    console.log("view scores button working")
-    beginQuizBtn.style.visibility = 'visible';
+
+
+
+function startTimer() {
+    timer = setInterval(function () {
+        timerCount--;
+        timeRemaining.textContent = "TIME REMAINING: " + timerCount;
+        if (timerCount === 0) {
+            clearInterval(timer);
+            console.log("you lose");
+        }
+    }, 1000);
 }
+
+
+
+// VIEW SCORES
+// viewScoresBtn.addEventListener("click", viewScores);
+// function viewScores() {
+//     console.log("view scores button working")
+// }
